434ad8fe10d4d727654e91cc269d1a4711dcbe59
lglayat <glayatle@g.cofc.edu>
2017-08-28 13:51:25 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  drake: 'draft day'
+}
+
+function updatePlaylist(playlist, artist_name, song_title){
+  playlist[artist_name] = song_title;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist_name){
+  delete playlist[artist_name];
+  return playlist;
+}
